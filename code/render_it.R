91fb75fdc96cfa26e4aec23eb9b775540cba86dc
McCrea Cobb <mccrea_cobb@fws.gov>
2020-03-04 10:38:47 -0900

    Adds images and render_it.R

--- code/render_it.R
+++ code/render_it.R
@@ -1,9 +1,6 @@
 ## Knit the rmd to the docs folder
 
-# Use moonreader
-xaringan:::inf_mr('./docs/ea_presentation.Rmd')
-
 # render it:
-rmarkdown::render(input='./docs/ea_presentation.Rmd',
+rmarkdown::render(input='./docs/r_github_presentation.Rmd',
                   output_file="index",
                   output_dir = "./docs")
